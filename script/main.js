939219039d32377af7e0f06a81616d9ddda4ef80
Sairam Suravarapu <31232128+sairam-suravarapu@users.noreply.github.com>
2020-12-29 19:52:28 +0530

    Update main.js

--- script/main.js
+++ script/main.js
@@ -106,8 +106,8 @@ const animationTimeline = () => {
     .from(".idea-2", 0.7, ideaTextTrans)
     .to(".idea-2", 0.7, ideaTextTransLeave, "+=1.5")
     .from(".idea-3", 0.7, ideaTextTrans)
-    .to(".idea-3 strong", 0.5, {
-      scale: 1.2,
+    .to(".idea-3 strong", 0.7, {
+      scale: 1.5,
       x: 10,
       backgroundColor: "rgb(21, 161, 237)",
       color: "#fff"
